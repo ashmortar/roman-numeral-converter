343811082fcfe2dc62320e2fca2c20d7a8512511
Aaron Ross and Glen Buck <aaron@>
2017-12-07 15:22:03 -0800

    simplified counter to reproduce same functions as previous numeralConverter

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //back end logic
  var numeralConverter = function(inputNumber) {
    var breakNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-   var numberSymbols = ["m", "mc", "d", "cd", "c", "xc", "l", "x", "ix", "v", "iv", "i"];
+   var numberSymbols = ["m", "mc", "d", "cd", "c", "xc", "l", "xl", "x", "ix", "v", "iv", "i"];
    var result = "";
+
+   if ((inputNumber <= 0)  || (inputNumber > 4000)) {
+     return false;
+   }
 
    for (var i = 0; i < 20; i ++) {
      for (var j = 0; j < breakNumbers.length; j++) {
-       if (inputNumber)
+       debugger;
+       if (inputNumber >= breakNumbers[j]) {
+         result += numberSymbols[j];
+         inputNumber = inputNumber - breakNumbers[j];
+         if ((inputNumber - breakNumbers[j]) === 0){
+           i = 18;
+         }
+        break;
+       }
      }
    }
-
-}
+   return result;
+};
 
 
 
